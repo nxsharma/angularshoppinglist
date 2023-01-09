7be8669905ebec1b65bd9ef0547135f0e3ee91e4
Nikhil Sharma <nikhil.sharma@northcare.com>
2023-01-09 14:17:57 -0600

    added ingredients to recipes with recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,28 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
+
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe','This is simply a test','https://www.cookipedia.co.uk/wiki/images/2/2d/Chicken_curry_%282%29_recipe.jpg'),
-        new Recipe('Another Recipe','This is simply a test','https://www.cookipedia.co.uk/wiki/images/2/2d/Chicken_curry_%282%29_recipe.jpg')
+        new Recipe('Biryani','This is simply a test','https://en.wikipedia.org/wiki/Biryani#/media/File:Biryani_of_Lahore.jpg',  
+         [
+            new Ingredient('Meat',1),
+            new Ingredient('Frendch Fries',20)
+        ]), 
+        new Recipe('Butter Chicken','This is simply a test','https://en.wikipedia.org/wiki/Butter_chicken#/media/File:Chicken_makhani.jpg',
+        [
+            new Ingredient('Buns',1),
+            new Ingredient('MEat Fries',20)
+        ])
+      
     ];
 
     getRecipes(){
         return this.recipes.slice(); //this way can't access recipes, but just the copies.
     }
+
+   
 }
