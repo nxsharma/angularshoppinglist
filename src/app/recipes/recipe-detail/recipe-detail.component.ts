345ee209e605602e50600333318ee17bcfb829f2
Nikhil Sharma <nikhil.sharma@northcare.com>
2023-03-08 17:00:01 -0600

    programmatic navigation to the edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -12,7 +12,8 @@ export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
   id: number;
   constructor(private recipeService: RecipeService,
-      private route: ActivatedRoute) { }
+      private route: ActivatedRoute,
+      private router:Router) { }
 
   ngOnInit(): void {
     this.route.params
@@ -27,4 +28,9 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
+  onEditRecipe(){
+     this.router.navigate(['edit'], {relativeTo: this. route});
+    //  this.router.navigate(['../', this.id, 'edit'], {relativeTo: this. route}); //more complex navigation
+
+  }
 }
