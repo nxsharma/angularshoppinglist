c55cc1bbd94c8463c01a9fc279ad59cd3d7c29fd
Nikhil Sharma <nikhil.sharma@northcare.com>
2023-12-22 10:33:36 -0600

    adding the token to outgoing requests

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,17 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-header',
     templateUrl:'./header.component.html'
 })
-export class HeaderComponent{
-    constructor(private dataStorageService: DataStorageService){}
+export class HeaderComponent implements OnInit, OnDestroy{
+    isAuthenticated = false;
+    private userSub: Subscription;
+    
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService){}
     collapsed = true;
     // @Output() featureSelected = new EventEmitter<string>();
 
@@ -14,6 +19,17 @@ export class HeaderComponent{
 
     //     this.featureSelected.emit(feature);
     // }
+
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe(user =>{
+            this.isAuthenticated = !user ? false: true;
+        });
+        
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
+    }
     onSaveData(){
         this.dataStorageService.storeRecipes();
     }
